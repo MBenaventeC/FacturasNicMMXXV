42c226fb29d6e07e95e6ca46dcef898003c7a135
IgnacioJamettR <ignacio.jamett@ug.uchile.cl>
2025-08-26 21:31:24 -0400

    XML Generator done (Before python team)
    We won

--- Java/SiiFact/XmlGenerator.java
+++ Java/SiiFact/XmlGenerator.java
@@ -3,6 +3,7 @@ import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
 import java.io.File;
 
+import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import java.util.GregorianCalendar;
@@ -10,9 +11,9 @@ import java.util.GregorianCalendar;
 import java.math.BigDecimal;
 
 public class XmlGenerator {
-    public static void main(String[] args) throws JAXBException {
+    public static void main(String[] args) throws JAXBException, DatatypeConfigurationException {
         // 1. Create and populate your object
-        Factura obj = new Factura();
+        SiiFact.Factura obj = new SiiFact.Factura();
         obj.setEmisor("NIC");
         obj.setReceptor("SII");
         GregorianCalendar calendar = new GregorianCalendar();
@@ -22,14 +23,14 @@ public class XmlGenerator {
         // ...set other fields...
 
         // 2. Initialize JAXBContext
-        JAXBContext context = JAXBContext.newInstance(Factura.class);
+        JAXBContext context = JAXBContext.newInstance(SiiFact.Factura.class);
 
         // 3. Create marshaller and enable pretty-print
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
         // 4. Marshal to a file or System.out
-        File output = new File("FacturaOutXD.xml");
+        File output = new File("Java/out/FacturaOutXD.xml");
         marshaller.marshal(obj, output);
         // or: marshaller.marshal(obj, System.out);
     }
