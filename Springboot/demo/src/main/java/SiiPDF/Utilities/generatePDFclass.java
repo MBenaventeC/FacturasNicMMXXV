9a37abd1d0e637b5a3f8260ad175c30041e4a713
Andrés Bravo <andresignaciobc@gmail.com>
2025-09-09 21:32:17 -0300

    Add Bases Generación PDF
    Extraídos ejemplos, clases y métodos del repo del NIC para transformarlos y utilizarlos con tecnologías actualizadas

---
+++ Springboot/demo/src/main/java/SiiPDF/Utilities/generatePDFclass.java
@@ -0,0 +1,66 @@
+package SiiPDF;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.sax.SAXResult;
+
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.MimeConstants;
+import org.apache.fop.apps.FOPException;
+
+public class generatePDFclass{
+    public static void generatePDF(InputStream xmlFile, InputStream xslFile,
+    			OutputStream pdfFile) throws FOPException, FileNotFoundException,
+    			TransformerException {
+    		FopFactory fopFactory = FopFactory.newInstance();   
+
+    		FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+    		// configure foUserAgent as desired 
+
+    		// Construct fop with desired output format
+    		Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent,
+    				pdfFile);   
+
+    		// Setup XSLT
+    		TransformerFactory factory = TransformerFactory.newInstance();
+    		Transformer transformer = factory.newTransformer(new StreamSource(
+    				xslFile));  
+
+    		// Set the value of a <param> in the stylesheet
+    		transformer.setParameter("versionParam", "2.0");    
+
+    		// Setup input for XSLT transformation
+    		Source src = new StreamSource(xmlFile);
+    		// Resulting SAX events (the generated FO) must be piped through to FOP
+    		Result res = new SAXResult(fop.getDefaultHandler());    
+
+    		// res = new StreamResult(new File("data/generated/sample-fo.xml"));    
+
+    		// Start XSLT transformation and FOP processing
+    		transformer.transform(src, res);
+    }
+    // Ejemplo de uso
+    public static void main(String[] args) throws Exception {
+        try (
+            InputStream xmlFile = new FileInputStream("test_files/test_xml3.xml");
+            InputStream xslFile = new FileInputStream("SiiPDF/plantilla_pdf.xsl");
+            OutputStream pdfFile = new FileOutputStream("test_files/test_pdf.pdf")
+        ) {
+            generatePDF(xmlFile, xslFile, pdfFile);
+            System.out.println("PDF generado correctamente.");
+        }
+    }
+
+}
