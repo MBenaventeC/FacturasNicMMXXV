75f02d56a87d33ec8c3a9e103499f222e8fea12b
andyreus17 <andyreus17@gmail.com>
2025-10-09 16:39:44 -0300

    Se actualiza FirmarXML.java

--- Java/src/main/java/FirmarXML.java
+++ Java/src/main/java/FirmarXML.java
@@ -15,6 +15,8 @@ import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -22,13 +24,13 @@ import javax.xml.transform.stream.StreamResult;
 public class FirmarXML {
     public static void main(String[] args) throws Exception {
 
-        // === 1. Cargar el archivo .p12 o .pfx ===
+        // 1) Cargar el archivo .p12 o .pfx ===
         /**
          * AÑADAN SU ARCHIVO .PFX A LA CARPETA JAVA
          * (CUIDADO CON SUBIRLA A GITHUB, PORFAVOR AÑADIRLA A SU GITIGNORE O ALGO)
          * Y PEGAR SU RUTA A rutaPFX
          */
-        String rutaPFX = "";
+        String rutaPFX = "C:\\universidad\\proyecto_de_software\\FacturasNicMMXXV\\Java\\certificado.pfx";
         /**
          * LA CONTRASEÑA ES LA MISMA QUE SE CREÓ CUANDO SE GENERÓ EL CERTIFICADO
          */
@@ -44,42 +46,54 @@ public class FirmarXML {
         PrivateKey privateKey = (PrivateKey) ks.getKey(alias, password.toCharArray());
         X509Certificate cert = (X509Certificate) ks.getCertificate(alias);
 
-        // === 2. Cargar el XML a firmar ===
+        // 2) Cargar el XML a firmar ===
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setNamespaceAware(true); // importante para firma XML
         Document doc = dbf.newDocumentBuilder().parse(new File("FACTURA_PARA_FIRMAR_TEST.xml"));
 
-        // === 3. Inicializar Apache Santuario ===
+        // Hallar el elemento Documento y asegurar el atributo ID como tipo ID
+        Element documento = (Element) doc.getElementsByTagName("Documento").item(0);
+        String docId = documento.getAttribute("ID");
+        if (docId.isEmpty()) {
+            throw new IllegalStateException("El <Documento> debe tener atributo ID.");
+        }
+        // Marcar atributo como tipo ID para Santuario
+        documento.setIdAttribute("ID", true);
+
+        // 3) Inicializar Apache Santuario ===
         Init.init();
 
-        // === 4. Crear la firma ===
+        // 4) Crear la firma ===
         String baseURI = new File("documento.xml").toURI().toString();
-        XMLSignature firma = new XMLSignature(doc, baseURI, XMLSignature.ALGO_ID_SIGNATURE_RSA_SHA256);
-
-        // Nodo raíz donde se insertará la firma
-        Element root = doc.getDocumentElement();
-        root.appendChild(firma.getElement());
+        XMLSignature firma = new XMLSignature(
+                doc,
+                baseURI,
+                XMLSignature.ALGO_ID_SIGNATURE_RSA_SHA1);
 
         // Transforms: indicar que se aplica canonización y referencia local
         Transforms transforms = new Transforms(doc);
         transforms.addTransform(Transforms.TRANSFORM_ENVELOPED_SIGNATURE);
-        transforms.addTransform(Transforms.TRANSFORM_C14N_EXCL_OMIT_COMMENTS);
+        transforms.addTransform(Transforms.TRANSFORM_C14N_OMIT_COMMENTS);
 
-        // Se puede firmar todo el documento o un nodo con ID específico
-        firma.addDocument("", transforms, MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA256);
+        // 5) Agregar referencia al nodo por ID y digest sha1
+        firma.addDocument("#" + docId, transforms, MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA1);
 
-        // === 5. Agregar información de la clave/certificado ===
+        // 6) Agregar información de la clave/certificado ===
         firma.addKeyInfo(cert);
         firma.addKeyInfo(cert.getPublicKey());
 
-        // === 6. Firmar el XML ===
+        // 7) Insertar <Signature> en <DTE> (hermano de <Documento>)
+        Element dte = (Element) doc.getElementsByTagName("DTE").item(0);
+        dte.appendChild(firma.getElement());
+
+        // 8) Firmar el XML ===
         firma.sign(privateKey);
 
-        // === 7. Guardar el XML firmado ===
-        FileOutputStream fos = new FileOutputStream("documento_firmado.xml");
-        TransformerFactory.newInstance().newTransformer()
-                .transform(new DOMSource(doc), new StreamResult(fos));
-        fos.close();
+        // 9) Serializar con ISO-8859-1 y saltos de línea y Guardar el XML firmado ===
+        Transformer tf = TransformerFactory.newInstance().newTransformer();
+        tf.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
+        tf.setOutputProperty(OutputKeys.INDENT, "yes");
+        tf.transform(new DOMSource(doc), new StreamResult(new File("DTE_firmado.xml")));
 
         System.out.println("✅ Documento firmado correctamente: documento_firmado.xml");
     }
